34df4bc4cade4e22363b6b0072b01297d9e2d7e4
Inero-dev <yytutu81@gmail.com>
2024-11-28 09:41:45 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [MenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'V1_GYM';
+}
